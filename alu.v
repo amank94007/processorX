6364f6ef788ec12e94be5c3fa9cd6fbb8bbdaa3d
aman <amank94007@gmail.com>
2019-07-21 15:09:55 +0530

    alu new opcode added

--- alu.v
+++ alu.v
@@ -1,87 +1,97 @@
-///have to change opcode only
+///have to change opcode 
+// LDA instruvtion to load in result 
+// A is input so take care of rotation and shifting operation 
 
 module alu(result,flags,A,B,opcode,clkout,cin,val,fl);
-	defparam data_size=32;
-	defparam MSB=data_size-1;
+	
+	///////////////////////////////////////////////
+	
+	parameter data_size = 32;
+	parameter MSB=data_size-1;
+	
+	///////////  PORTS  //////////////////////////
+	input cin,fl;
+	input clkout;
+	input [data_size-1:0] A,B,val;
+	input [7:0] opcode;
+	output reg [data_size-1:0] result;
+	output reg [4:0] flags=0;//[ZCSPV] Zero Carry Sign Parity oVerflow	
+	
+
 	///////////  OPCODE  /////////////////////////
-	defparam ADD=6'b001_001;
-	defparam ADI=6'b010_001;
-	defparam ACA=6'b110_001;
-	defparam ACI=6'b100_001;
-	defparam SUB=6'b001_010;
-	defparam SCI=6'b010_010;
-	defparam SBI=6'b001_010;
-	defparam SCA=6'b010_010;
-	defparam XNR=6'b001_110;
-	defparam XNI=6'b010_110;
-	defparam XOR=6'b001_011;
-	defparam XRI=6'b010_011;
-	defparam AND=6'b001_100;
-	defparam ANI=6'b010_100;
-	defparam ORA=6'b001_101;
-	defparam ORI=6'b010_101;
-	defparam NOT=6'b011_001;
-	defparam NEG=6'b011_000;
-	defparam SRL=6'b011_010;
-	defparam SLL=6'b011_011;
-	defparam ASR=6'b011_110;
-	defparam ASL=6'b011_110;
-	defparam SRC=6'b011_010;
-	defparam SLC=6'b011_011;
-	defparam ROR=6'b011_110;
-	defparam ROL=6'b011_110;
-	defparam RRC=6'b011_010;
-	defparam RLC=6'b011_011;
-	defparam CLR=6'b011_111;
-	defparam INC=6'b011_101;
-	defparam DEC=6'b011_100;
-	defparam POP=1;
-	defparam PSH=1;
-	defparam MVS=1;
-	defparam STS=1;
-	defparam CCP=1;
-	defparam JCP=1;
-	defparam CCD=1;
-	defparam JCD=1;
-	defparam RTC=1;
-	defparam JCA=1;
-	defparam MVP=1;
-	defparam RTU=1;
-	defparam CUA=1;
-	defparam STA=1;
-	defparam CLA=1;
-	defparam MVR=1;
-	defparam JUA=1;
-	defparam CUP=1;
-	defparam JUP=1;
-	defparam CUD=1;
-	defparam JUD=1;
-	defparam NOP=1;
+	parameter ADD=1;
+	parameter ADI=2;
+	parameter ACA=3;
+	parameter ACI=4;
+	parameter SUB=5;
+	parameter SCI=6;
+	parameter SBI=7;
+	parameter SCA=7;
+	parameter XNR=9;
+	parameter XNI=10;
+	parameter XOR=11;
+	parameter XRI=12;
+	parameter AND=13;
+	parameter ANI=14;
+	parameter ORA=15;
+	parameter ORI=16;
+	parameter NOT=17;
+	parameter NEG=18;
+	parameter SRL=19;
+	parameter SLL=20;
+	parameter ASR=21;
+	parameter ASL=22;
+	parameter SRC=23;
+	parameter SLC=24;
+	parameter ROR=25;
+	parameter ROL=26;
+	parameter RRC=27;
+	parameter RLC=28;
+	parameter CLR=29;
+	parameter INC=30;
+	parameter DEC=31;
+	parameter POP=32;
+	parameter PSH=33;
+	parameter MVS=34;
+	parameter STS=35;
+	parameter CCP=36;
+	parameter JCP=37;
+	parameter CCD=38;
+	parameter JCD=39;
+	parameter RTC=40;
+	parameter JCA=41;
+	parameter MVP=42;
+	parameter RTU=43;
+	parameter CUA=44;
+	parameter STA=45;
+	parameter CLA=46;
+	parameter MVR=47;
+	parameter JUA=48;
+	parameter CUP=49;
+	parameter JUP=50;
+	parameter CUD=51;
+	parameter JUD=52;
+	parameter NOP=53;
 	
 	
 	//////////////////////////////////////////////
-	defparam ssize=32;
-	defparam sptr_size=4;
+	parameter ssize=32;
+	parameter sptr_size=4;
 	reg [ssize-1:0] stack[(sptr_size**2)-1:0];			//stack memory
 	reg [sptr_size-1:0] sptr=(sptr_size**2);
 	reg stack_V=0,stack_U=0;
 	/////////////////////////////////////////////
-	defparam msize=32;
-	defparam mptr_size=4;
+	parameter msize=32;
+	parameter mptr_size=4;
 	reg [msize-1:0] mem[(mptr_size**2)-1:0];	  			//data memory 
 	reg [mptr_size-1:0] mptr=0;
 	/////////////////////////////////////////////
-	defparam isize=32;
-	defparam iptr_size=4;
+	parameter isize=32;
+	parameter iptr_size=4;
 	reg [isize-1:0] inst_reg[(iptr_size**2)-1:0];		//instruction memory 
 	reg [iptr_size-1:0] pc=0;
 	/////////////////////////////////////////////
-	input cin,fl;
-	input clkout;
-	input [7:0] opcode;
-	output reg [data_size-1:0] result;
-	output reg [4:0] flag=0;//[ZCSPV] Zero Carry Sign Parity oVerflow	
-
+	
 	always @(*)
 		begin
 		case(opcode)
@@ -89,7 +99,7 @@ module alu(result,flags,A,B,opcode,clkout,cin,val,fl);
 			///Stack and PC operations initally at top then decrement and insert data push operation
 			POP: //POP in stack
 				begin
-				 if(str==(sptr_size**2))
+				 if(sptr==(sptr_size**2))
 				 stack_U=1;//stack underflow
 				 else begin
 				 result=stack[sptr];
@@ -98,7 +108,7 @@ module alu(result,flags,A,B,opcode,clkout,cin,val,fl);
 				end
 			PSH: //PUSH in stack
 				begin
-				 if(str==0)	
+				 if(sptr==0)	
 				 stack_V=1;//overflow stack
 				 else begin
 				 sptr=sptr-1;
@@ -111,14 +121,16 @@ module alu(result,flags,A,B,opcode,clkout,cin,val,fl);
 				end
 			STS:
 				begin
-				A=sptr;
+				result=sptr;
 				end
 			CCP:
 				begin
-				if(fl==1)begin
+				if(fl==1)
+				begin
 				sptr=sptr-1;
 				sptr=pc;
-				pc=pc+val;end
+				pc=pc+val;
+				end
 				end
 			JCP:
 				begin
@@ -127,10 +139,12 @@ module alu(result,flags,A,B,opcode,clkout,cin,val,fl);
 				end
 			CCD:
 				begin
-				if(fl==1)begin
+				if(fl==1)
+				begin
 				sptr=sptr-1;
 				sptr=pc;
-				pc=val;end
+				pc=val;
+				end
 				end
 			JCD:
 				begin
@@ -139,9 +153,11 @@ module alu(result,flags,A,B,opcode,clkout,cin,val,fl);
 				end
 			RTC:
 				begin
-				if(fl==1)begin
+				if(fl==1)
+				begin
 				sptr=pc;
-				sptr=sptr+1;end
+				sptr=sptr+1;
+				end
 				end
 			JCA:
 				begin
@@ -189,20 +205,21 @@ module alu(result,flags,A,B,opcode,clkout,cin,val,fl);
 				end
 			
 			/////////////////////////////////////////
-			NOP: @(posedge clk) result=result;
+			
+			NOP: @(posedge clkout) result=result;
 			
 			STA: //store into memory
 				begin
 				 mem[mptr]=result;
 				 mptr=mptr+1;
 				end
-			LDA:	//Load from memory address is given
-				begin
-				 A=mem[mptr];
-				end
+			//LDA:	//Load from memory address is given
+				//begin
+				//A=mem[mptr];
+				//end
 			MVR:
 				begin
-				A=val;
+				//A=val;
 				end
 			ADD: //ADD
 				begin
@@ -306,17 +323,17 @@ module alu(result,flags,A,B,opcode,clkout,cin,val,fl);
 			ROL: //Rotate left without carry
 				begin
 				result <= @(posedge clkout)  A << 1;
-				A[0]<=A[MSB];
+				//A[0]<=A[MSB];
 				end
 			ROR: //rotate rt without carry
 				begin
 				result <= @(posedge clkout)  A >> 1;
-				A[MSB]<=A[0];
+				//A[MSB]<=A[0];
 				end
 			RLC: //Rotate left with carry
 				begin
 				result <= @(posedge clkout)  A << 1;
-				A[0]<=flags[3];	//carry
+				//A[0]<=flags[3];	//carry
 				end
 			RRC: //Rotate rt with carry
 				begin
